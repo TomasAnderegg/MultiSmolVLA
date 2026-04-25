fd1e3f49cd0c46334510b7aa0671ba0fcbaa1a41
TomasAnderegg <tjga.98@gmail.com>
2026-04-25 19:04:05 +0200

    all blocks seem fine

--- src/pipeline/imagebind_encoder.py
+++ src/pipeline/imagebind_encoder.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from imagebind.models import imagebind_model
 from imagebind.models.imagebind_model import ModalityType
+from torchvision.transforms.functional import normalize
 
 
 class ImageBindThermalEncoder(nn.Module):
@@ -16,12 +17,17 @@ class ImageBindThermalEncoder(nn.Module):
         print("[ImageBind] Loading ...")
         self.model = imagebind_model.imagebind_huge(pretrained=True)
         self.model.eval()
+        self.model.to(device)
         for p in self.model.parameters():
             p.requires_grad = False
         print("[ImageBind] Loaded ✅")
 
     def forward(self, thermal_images: torch.Tensor) -> torch.Tensor:
-        inputs = {ModalityType.VISION: thermal_images.to(self.device)}
+        x = normalize(thermal_images.to(self.device),
+                      mean=[0.48145466, 0.4578275, 0.40821073],
+                      std=[0.26862954, 0.26130258, 0.27577711],
+                    )
+        inputs = {ModalityType.VISION: x}
         with torch.no_grad():
             embeddings = self.model(inputs)
         out = embeddings[ModalityType.VISION]   # (B, 1024)
